d7f09b0156c2cb8c5ec1679f36b25f0e99f82c27
Dmitry Serpkov <dmi-serpkov@yandex.ru>
2020-04-11 22:21:47 +0300

    feat: добавить логику работы кнопок, readme

---
+++ script.js
@@ -0,0 +1,31 @@
+const editButton = document.querySelector('.profile__button_type_edit');
+let profileName = document.querySelector('.profile__name');
+const profileDescription = document.querySelector('.profile__description');
+const popupBlock = document.querySelector('.popup');
+const formElement = document.querySelector('.popup__container');
+const exitIcon = document.querySelector('.popup__close-icon');
+const inputName = popupBlock.querySelector('.popup__item_type_name');
+const inputDescription = popupBlock.querySelector('.popup__item_type_description');
+const saveButton = document.querySelector('.popup__button');
+
+editButton.addEventListener('click', showPopupForm);
+exitIcon.addEventListener('click', closeForm);
+formElement.addEventListener('submit', formSubmitHandler);
+
+function showPopupForm() {
+  popupBlock.classList.add('popup_opened');
+  inputName.value = profileName.textContent;
+  inputDescription.value = profileDescription.textContent;
+}
+
+function closeForm() {
+  popupBlock.classList.remove('popup_opened');
+}
+
+function formSubmitHandler (evt) {
+  evt.preventDefault();
+
+  profileName.textContent = inputName.value;
+  profileDescription.textContent = inputDescription.value;
+  closeForm();
+}
