78c7d34b98b344fce3fc587bb1f12bd7afe6e5fb
Dmitry Serpkov <dmi-serpkov@yandex.ru>
2020-04-13 11:48:49 +0300

    fix: стилизовать кнопки через миксы

--- script.js
+++ script.js
@@ -1,29 +1,22 @@
-const editButton = document.querySelector('.profile__button_type_edit');
+const editButton = document.querySelector('.profile__edit-button');
 const profileName = document.querySelector('.profile__name');
 const profileDescription = document.querySelector('.profile__description');
 const popupBlock = document.querySelector('.popup');
 const formElement = document.querySelector('.popup__container');
-const exitIcon = document.querySelector('.popup__close-icon');
+const exitIcon = document.querySelector('.popup__close-button');
 const inputName = popupBlock.querySelector('.popup__item_type_name');
 const inputDescription = popupBlock.querySelector('.popup__item_type_description');
 const saveButton = document.querySelector('.popup__button');
-let closedStatus = true;
+
 
 editButton.addEventListener('click', showClosePopupForm);
 exitIcon.addEventListener('click', showClosePopupForm);
 formElement.addEventListener('submit', formSubmitHandler);
 
 function showClosePopupForm() {
-  if (closedStatus === true) {
-    popupBlock.classList.add('popup_opened');
-    inputName.value = profileName.textContent;
-    inputDescription.value = profileDescription.textContent;
-    closedStatus = false;
-  }
-  else {
-    popupBlock.classList.remove('popup_opened');
-    closedStatus = true;
-  }
+  inputName.value = profileName.textContent;
+  inputDescription.value = profileDescription.textContent;
+  popupBlock.classList.toggle('popup_opened');
 }
 
 function formSubmitHandler (evt) {
